8977ebd0d2983857eddfbd48544e03a3b2a3e76f
ilcors-dev <corsetti.luca28@gmail.com>
2022-12-15 19:49:41 +0100

    chore: font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'source-code-pro': ['Source Code Pro', 'Roboto', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
