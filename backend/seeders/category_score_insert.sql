aa0617a5eb89fd1fb5a655c460587de7fdddf556
ilcors-dev <corsetti.luca28@gmail.com>
2022-12-22 14:30:45 +0100

    chore: added 'sec' questions & score settings in database

---
+++ backend/seeders/category_score_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO category_score_settings (answer_type,score,category_id) VALUES
+    ('correct', 0.85, 1),
+    ('wrong', 0.64, 1),
+    ('empty', 0, 1),
+    ('correct', 1, 2),
+    ('wrong', 0.85, 2),
+    ('empty', 0, 2);
+
