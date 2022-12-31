12de7c9664b4f407aa90dc9321815b4f856d4f9c
Luca Corsetti <corsetti.luca28@gmail.com>
2022-12-31 12:39:18 +0100

    fix: questions answers

--- backend/seeders/category_score_insert.sql
+++ backend/seeders/category_score_insert.sql
@@ -2,7 +2,7 @@ INSERT INTO category_score_settings (answer_type,score,category_id) VALUES
     ('correct', 0.85, 1),
     ('wrong', 0.64, 1),
     ('empty', 0, 1),
-    ('correct', 1, 2),
-    ('wrong', 0.85, 2),
+    ('correct', 85, 2),
+    ('wrong', 0.64, 2),
     ('empty', 0, 2);
 
