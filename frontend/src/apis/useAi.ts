71426a06f9b72d0ea915c8c8394caab396466c62
ilcors-dev <corsetti.luca28@gmail.com>
2025-01-09 15:17:41 +0100

    perf: clearer instructions for ai + support latex

--- frontend/src/apis/useAi.ts
+++ frontend/src/apis/useAi.ts
@@ -17,7 +17,17 @@ const api = {
 			body: JSON.stringify({
 				model: 'llama3.2',
 				prompt:
-					"Given this question with its answer(s) (both correct and incorrect), explain why the answer(s) is/are correct in a coincise way using markdown syntax (use lists, font styles, do not use headers as it should be included in a written paragraph). Also briefly explain why the other answers are incorrect..\n\nHere's the question+answers (the correct ones are passed in the corresponding property):\n" +
+					"INSTRUCTIONS:\
+- Given this question with its answer(s) (both correct and incorrect) explain why the answer(s) is/are correct in a coincise way.\
+- Include the theory behind the correct answer.\
+- Explain first the correct answers, the explain why the other are false.\
+- Be concise and clear, important! Avoid wall of texts too lengthy to read.\
+STYLE GUIDE:\
+- Use markdown syntax and lists to explain the concepts.\
+- Use font styles\
+- Do not use headers as it should be included in a written paragraph.\
+- Use also latex where appropriate for math formulas when needed.\
+\n\nHere's the question+answers (the correct ones are passed in the corresponding property):\n" +
 					text,
 				stream: false,
 			}),
